1878050269e01fb4761e528bac80da036f0025f3
yon <Lighthawky@gmail.com>
2012-10-22 12:03:29 +0200

    added log support

---
+++ lib/controller_supports/log_support.rb
@@ -0,0 +1,37 @@
+require 'active_support/concern'
+module Log
+  module ControllerSupport
+
+    extend ActiveSupport::Concern
+
+    included do
+      before_filter :set_exceptional_context if respond_to? :before_filter
+      alias_method_chain :handle_unverified_request, :log
+    end
+
+    def set_exceptional_context
+      context = {}
+      context.merge!(
+          :url => request.url,
+          :user_agent => request.user_agent,
+          :ip => request.ip,
+          :remote_ip => request.remote_ip,
+          :referer => request.referer,
+          :environment => Rails.env.to_s,
+          :session_id => request.session_options[:id],
+          :session => session,
+          :params => params
+      )
+      context.merge!(:current_user => current_user.try(:id)) if respond_to? :current_user
+      context.merge!(more_context) if respond_to? :more_context
+      Log.clear_context
+      Log.context(context)
+    end
+
+    def handle_unverified_request_with_log
+      Log.warn("Can't verify CSRF token authenticity")
+      handle_unverified_request_without_log
+    end
+
+  end
+end
